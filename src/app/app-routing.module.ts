31f1c34cc19349ad2986842ac3ed6304069c5ad0
srueda <srueda@ec.krugercorp.com>
2024-05-13 19:42:06 -0500

    first configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+/**
+ * Routing module udinf Lazy loading
+ *
+ * @author components
+ * @version 1.0
+ * @since 1.0.0
+ */
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  // {
+  //   path: 'profiles',
+  //   // canActivate: [CanActivateGuard],
+  //   loadChildren: () => import('./modules/profiles/profiles.module').then(m => m.ProfilesModule)
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponentComponent } from './common-components/header-component/header-component.component';
+import { ImageUploaderComponent } from './common-components/image-uploader/image-uploader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponentComponent,
+    ImageUploaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
