bab3ed916813ff4ddca4cc13b0b51d65498b1414
srueda <sebas18rueda@gmail.com>
2024-09-03 21:15:10 -0500

    doing some test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FileUploadModule } from 'primeng/fileupload';
 import { HeaderComponentComponent } from './common-components/header-component/header-component.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ImageUploaderComponent } from './common-components/image-uploader/image-uploader.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +20,12 @@ import { ImageUploaderComponent } from './common-components/image-uploader/image
     ImageUploaderComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    FileUploadModule,
+    HttpClientModule,
+    InputTextModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common-components/header-component/header-component.component.ts
+++ src/app/common-components/header-component/header-component.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-header-component',
   templateUrl: './header-component.component.html',
-  styleUrls: ['./header-component.component.css']
+  styleUrls: ['./header-component.component.css'],
 })
 export class HeaderComponentComponent implements OnInit {
+
+  items: MenuItem[] = [];
+  sideItems: MenuItem[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+      },
+      {
+        label: 'Events',
+        icon: 'pi pi-fw pi-calendar',
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {
+            label: 'Left',
+            icon: 'pi pi-fw pi-align-left'
+          },
+          {
+            label: 'Right',
+            icon: 'pi pi-fw pi-align-right'
+          },
+          {
+            label: 'Center',
+            icon: 'pi pi-fw pi-align-center'
+          },
+          {
+            label: 'Justify',
+            icon: 'pi pi-fw pi-align-justify'
+          },
+
+        ]
+      },
+    ];
+
+    this.sideItems =  [
+      {
+        items: [
+            {
+                label: 'Profile',
+                icon: 'pi pi-profile'
+            },
+            {
+                label: 'Log out',
+                icon: 'pi pi-sign-out'
+            }
+        ]
+    }
+    ];
+
   }
 
 }
